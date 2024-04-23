d3392207886b0d75089249142e301ef8d3f62629
Brandon Jones <brandon.jones@wundermanthompson.com>
2024-04-23 13:06:44 -0400

    added files

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {createSession} from './api/influizeAPI';
+
 
 function App() {
+
+  const [session, setSession] = useState<any>(0);
+  
+  setSession(createSession(1));
+  console.log(session);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
