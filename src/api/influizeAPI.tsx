1890c0f47373195dcb718957a9a8f0daeeac1c6d
Brandon Jones <brandon.jones@wundermanthompson.com>
2024-04-23 13:51:16 -0400

    remove no-cors

--- src/api/influizeAPI.tsx
+++ src/api/influizeAPI.tsx
@@ -5,7 +5,6 @@ const api_key = process.env.API_KEY;
 export function createSession(flow_id:Number){
     var session_data = fetch(`https://mvp-api.influize.io/v0/flow/session?flow_id=${flow_id}`, {
         method:'POST',
-        mode: 'no-cors',
         headers: {
             'accept': 'application/json',
             'Authorization':  `Bearer ${api_key}`
